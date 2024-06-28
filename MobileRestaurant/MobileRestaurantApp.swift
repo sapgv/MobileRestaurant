c6a1ab2179919d9765a11c14b9416952a1e234dd
Grigory Sapogov <grisha.sapgv@mail.ru>
2024-06-28 21:12:56 +0500

    Initial Commit

---
+++ MobileRestaurant/MobileRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileRestaurantApp.swift
+//  MobileRestaurant
+//
+//  Created by Grigory Sapogov on 28.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MobileRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
